7b72917cb96bd18668f02b5493577fc0e2a6f5f5
Maxim Hrunenko <maxhrunenko@gmail.com>
2023-12-16 22:56:28 +0200

    dir list

---
+++ code/headers/dirlist.h
@@ -0,0 +1,37 @@
+#ifndef _DIR_LIST_
+#define _DIR_LIST_
+
+#define T_EXECUTABLE 1
+#define T_DIR 2
+#define T_READABLE 3
+#define B_MAX_SIZE 20
+
+
+struct dir_list {
+    char type;
+    char *file_name;
+};
+
+typedef struct d_file_list {
+    struct dir_list **files;
+    unsigned int len; 
+    unsigned int max_size;
+}D_LIST;
+
+
+typedef struct copy_file {
+    char **file_path_buf;
+    unsigned int buf_len;
+    char *file_path;
+} copy_buff;
+
+
+D_LIST *create_dirlist();
+void add_file(D_LIST *d_list, char l_type, char *l_file_name);
+
+
+void destroy_dirlist(D_LIST *list);
+
+
+
+#endif // _DIR_LIST_
---
+++ code/src/dirlist.c
@@ -0,0 +1,1 @@
+#include "../headers/dirlist.h"
